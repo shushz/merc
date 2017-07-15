f4d0ccc2b359e98647aa42c9595621172410d4ef
Alex Dvinsky <advinsky@fb.com>
2017-07-15 03:53:50 -0700

    Timing on debugLog

--- src/debugLog.js
+++ src/debugLog.js
@@ -5,9 +5,15 @@
  * @format
  */
 
+let lastMessage = 0;
+
 export default function debugLog(...args: Array<any>): void {
   if (process.env.DEBUG) {
+    const now = Date.now();
+    const time = now - lastMessage;
+    lastMessage = now;
+
     // eslint-disable-next-line no-console
-    console.log(...args);
+    console.log(`(${time})`, ...args);
   }
 }
